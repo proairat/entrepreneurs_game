b25b5b4ceabe96fc0398cdd887ff0eaad2ae7801
Shayakhmetov Airat Irekovich <proairat@gmail.com>
2023-02-24 21:42:46 +0500

    GPFED-59/fix: исправил ошибки с загрузкой формы и файла на сервер на фронтенде

--- packages/frontend/src/components/AppFetch.vue
+++ packages/frontend/src/components/AppFetch.vue
@@ -1,17 +1,10 @@
 <template>
   AppFetch
-  <div>isFinished {{ isFinished }}</div>
   <div>data {{ data }}</div>
-  <div>error {{ error }}</div>
 </template>
 
 <script setup lang="ts">
 import { useFetchComposable } from "@/composables/use-fetch";
 
-const { isFinished, data, error } = useFetchComposable(
-  "/notes",
-  "0",
-  "GET",
-  null
-);
+const { data } = useFetchComposable("/notes", "0", "GET", null);
 </script>
