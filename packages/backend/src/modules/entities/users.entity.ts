1166c48476c9fb5026336d0bceb20f8e3e17c359
Shayakhmetov Airat Irekovich <proairat@gmail.com>
2023-03-04 22:03:29 +0500

    GPFED-62/fix: на бакенде исправил ошибки, которые выдаёт npm run lint

--- packages/backend/src/modules/entities/users.entity.ts
+++ packages/backend/src/modules/entities/users.entity.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { EEntityType} from "@app/enums";
+import { EEntityType } from "@app/enums";
 
 @Entity()
 export class Users {
